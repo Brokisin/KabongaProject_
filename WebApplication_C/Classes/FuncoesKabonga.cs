823b77171ad8b47f46f92a1e05776aeb85101846
davi mica <davimica04@outlook.com>
2022-08-06 14:54:03 -0300

    Cadastro

---
+++ WebApplication_C/Classes/FuncoesKabonga.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication_C.Classes
+{
+    public static class FuncoesKabonga
+    {
+        public static User usuarioAtivo;
+
+    }
+}
